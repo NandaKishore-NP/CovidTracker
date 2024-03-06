4c93ad8e7bf28ee73067eff5052329db5aecae95
sneharaj4103 <sneharaj4103@gmail.com>
2024-03-06 11:50:05 +0530

    Datewise search

--- app.js
+++ app.js
@@ -23,6 +23,25 @@ app.get('/', (req, res) => {
   res.render('home', { user: req.session.user || null });
 });
 
+//Dateaise Search;
+app.get("/datewise",async(req,res)=>{
+ res.render("datewise_data")
+  })
+
+app.get("/datewise/:date",async(req,res)=>{
+  const requesteDate=new date(req.params.date)
+  try{
+    const data= await covid.find({covid:requesteDate})
+    if (!covid) {
+      return res.status(404).json({ message: 'No COVID data found for the given date' });
+  }
+
+  res.json(data);
+} catch (error) {
+  res.status(500).json({ message: 'Internal server error' });
+}
+});
+
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
